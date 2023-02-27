26d3eef438da78518e79a67da508858ece36669c
JunRu-Louis-Huang <s16981058@gmail.com>
2023-02-27 23:44:58 +0800

    modify working status and OPENAI_MAX_TOKENS

--- api/index.py
+++ api/index.py
@@ -39,18 +39,18 @@ def handle_message(event):
     if event.message.type != "text":
         return
 
-    if event.message.text == "說話":
+    if event.message.text == "Hi Louis!!":
         working_status = True
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text="我可以說話囉，歡迎來跟我互動 ^_^ "))
         return
 
-    if event.message.text == "閉嘴":
+    if event.message.text == "請暫停聊天":
         working_status = False
         line_bot_api.reply_message(
             event.reply_token,
-            TextSendMessage(text="好的，我乖乖閉嘴 > <，如果想要我繼續說話，請跟我說 「說話」 > <"))
+            TextSendMessage(text="好的，我暫停說話 > <，如果想要我繼續說話，請跟我說 「Hi Louis!!」 > <"))
         return
 
     if working_status:
